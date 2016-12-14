c22212715da12c969969208734201073631896eb
Vlad <micuvld@gmail.com>
2016-12-14 19:17:42 +0200

    First commit

---
+++ src/main/java/ConnectServlet.java
@@ -0,0 +1,58 @@
+
+import com.sun.net.httpserver.HttpServer;
+import ldapConnection.Connector;
+import org.forgerock.opendj.ldap.Entry;
+import org.forgerock.opendj.ldap.LdapException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+/**
+ * Created by vlad on 03.12.2016.
+ */
+// The Java class will be hosted at the URI path "/helloworld"
+@WebServlet("/connect")
+public class ConnectServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String rightsRequesterDn = request.getParameter("requesterDn");
+        String targetDn = request.getParameter("targetDn");
+        Client client = ClientBuilder.newClient();
+        JSONParser parser = new JSONParser();
+
+        String entity = client.target("http://localhost:18080/SecurityProvider")
+                .path("rights")
+                .queryParam("requesterDn", "cn=gateway1,ou=Gateways,dc=spital,dc=moinesti,dc=com")
+                .queryParam("rightsRequesterDn", rightsRequesterDn)
+                .queryParam("targetDn", targetDn)
+                .request()
+                .get(String.class);
+
+        System.out.println(entity);
+
+        try {
+            JSONObject rights = (JSONObject)parser.parse(entity);
+            response.getWriter().write(rights.toJSONString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vlad
+  Date: 03.12.2016
+  Time: 16:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
