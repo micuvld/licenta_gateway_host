38ebf2fb989779df80503d54cc0f9da929f19d31
Vlad <micuvld@gmail.com>
2017-03-13 14:43:43 +0200

    Moving to lenovo

--- src/main/java/ConnectServlet.java
+++ src/main/java/ConnectServlet.java
@@ -1,22 +1,14 @@
 
-import com.sun.net.httpserver.HttpServer;
-import ldapConnection.Connector;
-import org.forgerock.opendj.ldap.Entry;
-import org.forgerock.opendj.ldap.LdapException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
